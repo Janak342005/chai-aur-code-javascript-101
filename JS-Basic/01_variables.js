bdeced611b07b55a769bde0191f893f65426e501
Janak J Prajapati <133052681+Janak342005@users.noreply.github.com>
2023-11-08 12:19:14 +0000

    Varibles part is done use them in Ecome

---
+++ JS-Basic/01_variables.js
@@ -0,0 +1,18 @@
+// General Review of declarations
+const name = "Janak";
+const dob = "03/04/2005";
+let accEmaiId = "janak3405@janak.com";
+var accPassword = "janak34ngddj";
+
+console.log(name);
+console.log(dob);
+console.log(accEmaiId);
+
+/*
+1.  Never use var because it has issues with block scope and  functional scope.
+2.  variable memorey allocation can also be done without using let or var but it's not good to use it.
+3.  const are variables whome we can't redclare or change its values | EX :- Name DOB Surname parents-name this things wuil never change in genera case.
+4. Aways use let since it doesnt have probems with scope
+5. do add comment use // or multi comment ike this one
+
+*/
